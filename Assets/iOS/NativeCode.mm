c0b36ad6418014f811ce43ac5f06c41fe9ecdd16
stopiccot <alexey.petruchik@gmail.com>
2019-10-16 14:20:39 +0300

    Compilation fix

--- Assets/iOS/NativeCode.mm
+++ Assets/iOS/NativeCode.mm
@@ -1,5 +1,8 @@
+#import "CustomViewController.h"
+#import "UnityAppController.h"
+
 extern "C" void IOS13BannerTestScript_ShowController() {
-	NSLog(@"IOS13BannerTestScript_ShowController");
+    NSLog(@"IOS13BannerTestScript_ShowController");
     CustomViewController* controller = [[CustomViewController alloc] init];
     [[GetAppController() rootViewController] presentModalViewController:controller animated:YES];
 }
